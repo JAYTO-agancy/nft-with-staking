489a082968a791f0adbdb2f7cfc64841d5db8e35
Grigory <grinmorg@gmail.com>
2025-07-20 09:20:38 +0300

    fix(app): change project name

--- app/src/shared/lib/seo.ts
+++ app/src/shared/lib/seo.ts
@@ -1,4 +1,4 @@
-const SITE_NAME = "Nifty Forgs";
+const SITE_NAME = "Nifty Frogs";
 export function getSiteTitle(text?: string) {
   const title = text ? `${SITE_NAME} - ${text}` : SITE_NAME;
   return title;
