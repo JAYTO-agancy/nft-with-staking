42215dcf7bffbc2055c534e153c1b945fdcf00bf
Grigory <grinmorg@gmail.com>
2025-07-25 17:56:21 +0300

    feat(ui): update project name to 'Plumffel NFT' in header and footer logo

--- app/src/shared/ui/logo.tsx
+++ app/src/shared/ui/logo.tsx
@@ -16,7 +16,7 @@ export const LogoLink: React.FC<Props> = ({ className }) => {
       <span className="inline-block size-8">
         <Image src="/img/logo.png" width={32} height={32} alt="logo" />
       </span>
-      <span className="text-3xl leading-0 font-bold">NiftyFrogToken</span>
+      <span className="text-3xl leading-0 font-bold">Plumffel NFT</span>
     </Link>
   );
 };
